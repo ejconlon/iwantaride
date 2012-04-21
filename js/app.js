d52cadc1e92d88e1a30d1baa040acb9c8bd5bfad
Nicholas Stock <stockn@gmail.com>
2012-04-21 11:39:30 -0700

    adding beachflag

--- js/app.js
+++ js/app.js
@@ -19,17 +19,45 @@ function set_lat_lng() {
     }
 }
 
-function draw_map() {
-  var myOptions = {
-    zoom: 12,
-    center: new google.maps.LatLng(lat, lng),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+function my_map(){
+	var t = this;
+	function set_lat_lng() {
+	    if (google.loader.ClientLocation) {
+		lat = google.loader.ClientLocation.latitude;
+		lng = google.loader.ClientLocation.longitude;
+	    } else {
+		debug('Location set manually');
+		lat = '36.9742';
+		lng = '-122.0297';
+	    }
+	}
+	function overlayPoint(){
+		var image = 'img/beachflag.png';
+	    var myLatLng = new google.maps.LatLng(lat, lng);
+	    var beachMarker = new google.maps.Marker({
+	        position: myLatLng,
+	        map: t.map,
+	        icon: image
+	    });
+	}
+	function draw_map(){
+		var myOptions = {
+	    zoom: 12,
+	    center: new google.maps.LatLng(lat, lng),
+	    mapTypeId: google.maps.MapTypeId.ROADMAP
+	  }
+	  return new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	}
+	
+	set_lat_lng();
+	this.map = draw_map();
+	overlayPoint();
+	
 }
 
+
 $(document).ready(function() {
-    set_lat_lng();
-    draw_map();
+	
+	new my_map();
 });
 

2a786285e731ce2c2ad3ddd2adc8729ea0bb1c9d
Eric Conlon <ejconlon@gmail.com>
2012-04-21 02:20:01 -0700

    fix in firefox

--- js/app.js
+++ js/app.js
@@ -1,20 +1,10 @@
 function initialize() {
   var myOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 12,
+    center: new google.maps.LatLng(36.9742, -122.0297),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 }
 
-// TODO for some reason document.body.appendChild erases... everything...
-// non-lazy load it for now
-//function loadScript() {
-//  var script = document.createElement("script");
-//  script.type = "text/javascript";
-//  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBj3Cz1ShYUnODXeZH8IDll6rNq_GTFH6E&sensor=true"
-//  document.body.appendChild(script);
-//}
-//window.onload = loadScript;
-
-document.body.onload = initialize;
+window.onload = initialize;

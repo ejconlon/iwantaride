3321e8bf515275aeb03488e672a6e285bedb3558
Eric Conlon <ejconlon@gmail.com>
2012-04-21 08:14:18 -0700

    adding user login (no sessions) and improved views

--- app.py
+++ app.py
@@ -7,12 +7,38 @@ GMAPS_API_KEY = "AIzaSyBj3Cz1ShYUnODXeZH8IDll6rNq_GTFH6E"
 GMAPS_SRC="http://maps.googleapis.com/maps/api/js?key=%s&sensor=true" % GMAPS_API_KEY
 # TODO follow along https://developers.google.com/maps/documentation/javascript/tutorial
 
-import os, urlparse
+SALT="$JSFJF$J@NNSj4SFj2F@t5m5@5jfk@@SMSMCO"
+
+import os, urlparse, sha
 from bottle import *
 import redis
 
 # set in main block
 REDIS = None
+
+def hash_password(s):
+    return sha.sha(s+SALT).hexdigest()
+
+def user_exists(email):
+    uid = REDIS.get('email:%s:uid' % email)
+    return uid is not None
+
+def find_user(email, pwhash):
+    uid = REDIS.get('email:%s:uid' % email)
+    if uid:
+        stored_hash = REDIS.get('pwhash:%s' % uid)
+        if stored_hash == pwhash:
+            return uid
+    return None
+
+def add_user(email, pwhash):
+    if user_exists(email):
+        return None
+    else:
+        uid = REDIS.incr('global:uid_source')
+        REDIS.set('email:%s:uid' % email, uid)
+        REDIS.set('pwhash:%s' % uid, pwhash)
+        return uid
 
 ############
 # Static resource handlers
@@ -41,7 +67,51 @@ def img_static(filename):
 @route("/")
 @view("main")
 def landing_page():
-    return dict(title = "I Want A Ride!", content = "Where are you headed?", show_map=True)
+    return dict(text = "Where are you headed?")
+
+@route("/login")
+@view("login")
+def login():
+    return dict()
+
+@route("/verify_login", method="POST")
+def verify_login():
+    email = request.forms.get('email')
+    password = request.forms.get('password')
+    pwhash = hash_password(password)
+
+    if len(email) == 0 or len(password) == 0:
+        return template("login", {"error": "Fill in all the fields, thanks." })
+
+    uid = find_user(email, pwhash)
+    if uid is None:
+        return template("login", {"error": "We couldn't log you in with those credentials."})
+    else:
+        return template("main", {"success": "Thanks for logging in.", "text": "Where are you headed?"})
+
+@route("/signup")
+@view("signup")
+def signup():
+    return dict()
+
+@route("/verify_signup", method="POST")
+def verify_signup():
+    email = request.forms.get('email')
+    password = request.forms.get('password')
+    password_again = request.forms.get('password_again')
+
+    if len(email) == 0 or len(password) == 0 or len(password_again) == 0:
+        return template("signup", {"error": "Fill in all the fields, thanks." })
+    elif password != password_again:
+        return template("signup", {"error": "Your passwords don't match."})
+    
+    pwhash = hash_password(password)
+    uid = add_user(email, pwhash)
+    if uid:
+        return template("main", {"success": "Thanks for signing up.", "text": "Where are you headed?"})
+    else:
+        return template("signup", {"error": "That email is already registered."})
+    
 
 ###########
 # Dump db info
@@ -58,6 +128,10 @@ def db_get(item):
 @route("/db/set/:item/:value")
 def db_set(item, value):
     return render_row(REDIS.set(item, value))
+
+@route("/db/hash/:item")
+def db_hash(item):
+    return render_row(hash_password(item))
 
 if __name__ == "__main__":
     if os.environ.has_key('REDISTOGO_URL'):
--- js/app.js
+++ js/app.js
@@ -1,27 +1,35 @@
-function initialize() {
+var DEBUG_ENABLED = false;
+var lat = null;
+var lng = null;
+
+function debug(msg) {
+    if (DEBUG_ENABLED) {
+	alert('DEBUG: '+msg);
+    }
+}
+
+function set_lat_lng() {
+    if (google.loader.ClientLocation) {
+	lat = google.loader.ClientLocation.latitude;
+	lng = google.loader.ClientLocation.longitude;
+    } else {
+	debug('Location set manually');
+	lat = '36.9742';
+	lng = '-122.0297';
+    }
+}
+
+function draw_map() {
   var myOptions = {
     zoom: 12,
-    center: new google.maps.LatLng(36.9742, -122.0297),
+    center: new google.maps.LatLng(lat, lng),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 }
 
-
-var geocoder = null;
-var lat = null;
-var lng = null;
-
 $(document).ready(function() {
-    if (google.loader.ClientLocation) {
-	lat = google.loader.ClientLocation.latitude;
-	lng = google.loader.ClientLocation.longitude;
-    } else {
-	alert('DEBUG: Location set manually');
-	lat = '36.9742';
-	lng = '-122.0297';
-    }
-
-    initialize();
+    set_lat_lng();
+    draw_map();
 });
 

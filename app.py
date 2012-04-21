26dd949f5217fd0a6fecf4cb6d15ff7700024a29
Eric Conlon <ejconlon@gmail.com>
2012-04-20 23:35:07 -0700

    adding redis support, updating templates

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
 import os
-from bottle import route, run, static_file, template, view
+from bottle import *
+import redis
+from bottle.ext import redis as redis_plugin
+
+############
+# Static resource handlers
+# put things in /js /img /css and reference them in view templates
 
 @route('/js/<filename>')
 def js_static(filename):
-    return static_file(filename, root='./js')
+     return static_file(filename, root='./js')
 
 @route('/img/<filename>')
 def img_static(filename):
@@ -15,11 +21,36 @@ def img_static(filename):
 def img_static(filename):
     return static_file(filename, root='./css')
 
+###########
+# Main page
+
 @route("/")
 @view("main")
-def hello():
-    return dict(title = "Hello", content = "Hello from Python!")
+def main():
+    return dict(title = "I Want A Ride!", content = "Where are you headed?")
+
+###########
+# Dump db info
+
+def render_row(row):
+    if row:
+        return template('dump', row=row)
+    return HTTPError(404, "Page not found")    
+
+@route("/db/get/:item")
+def db_get(item, rdb):
+    return render_row(rdb.get(item))
+
+@route("/db/set/:item/:value")
+def db_set(item, value, rdb):
+    return render_row(rdb.set(item, value))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    #redis_port = int(os.environ.get("REDIS_PORT", 6379))
+    redis_host = os.environ.get("REDIS_HOST", "localhost")
+
+    app = default_app()
+    plugin = redis_plugin.RedisPlugin(host=redis_host)
+    app.install(plugin)
     run(host='0.0.0.0', port=port)
--- views/main.tpl
+++ views/main.tpl
@@ -38,12 +38,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-          <a class="brand" href="#">Project name</a>
+          <a class="brand" href="#">I Want A Ride!</a>
           <div class="nav-collapse">
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <!--<li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>-->
             </ul>
           </div><!--/.nav-collapse -->
         </div>

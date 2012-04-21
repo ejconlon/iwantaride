176ce1c7870ee7af835e035c6062004fb4f15e0e
Eric Conlon <ejconlon@gmail.com>
2012-04-21 08:58:33 -0700

    add logout

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ SALT="$JSFJF$J@NNSj4SFj2F@t5m5@5jfk@@SMSMCO"
 
 import os, urlparse, sha
 from bottle import *
+from beaker.middleware import SessionMiddleware
 import redis
 
 # set in main block
@@ -40,6 +41,28 @@ def add_user(email, pwhash):
         REDIS.set('pwhash:%s' % uid, pwhash)
         return uid
 
+def get_session():
+    return request.environ.get('beaker.session')
+
+def clear_session():
+    s = get_session()
+    del s['email']
+    del s['uid']
+    s.save()
+    return s
+
+def update_session(email, uid):
+    s = get_session()
+    s['email'] = email
+    s['uid'] = uid
+    s.save()
+    return s
+
+def session_dict(**kwargs):
+    d = { "session": get_session() }
+    d.update(kwargs)
+    return d
+
 ############
 # Static resource handlers
 # put things in /js /img /css and reference them in view templates
@@ -67,12 +90,12 @@ def img_static(filename):
 @route("/")
 @view("main")
 def landing_page():
-    return dict(text = "Where are you headed?")
+    return session_dict(text = "Where are you headed?")
 
 @route("/login")
 @view("login")
 def login():
-    return dict()
+    return session_dict()
 
 @route("/verify_login", method="POST")
 def verify_login():
@@ -81,18 +104,19 @@ def verify_login():
     pwhash = hash_password(password)
 
     if len(email) == 0 or len(password) == 0:
-        return template("login", {"error": "Fill in all the fields, thanks." })
+        return template("login", session_dict(error = "Fill in all the fields, thanks."))
 
     uid = find_user(email, pwhash)
     if uid is None:
-        return template("login", {"error": "We couldn't log you in with those credentials."})
+        return template("login", session_dict(error = "We couldn't log you in with those credentials."))
     else:
-        return template("main", {"success": "Thanks for logging in.", "text": "Where are you headed?"})
+        update_session(email, uid)
+        return template("main", session_dict(success = "Thanks for logging in.", text = "Where are you headed?"))
 
 @route("/signup")
 @view("signup")
 def signup():
-    return dict()
+    return session_dict()
 
 @route("/verify_signup", method="POST")
 def verify_signup():
@@ -101,17 +125,22 @@ def verify_signup():
     password_again = request.forms.get('password_again')
 
     if len(email) == 0 or len(password) == 0 or len(password_again) == 0:
-        return template("signup", {"error": "Fill in all the fields, thanks." })
+        return template("signup", session_dict(error = "Fill in all the fields, thanks."))
     elif password != password_again:
-        return template("signup", {"error": "Your passwords don't match."})
+        return template("signup", session_dict(error ="Your passwords don't match."))
     
     pwhash = hash_password(password)
     uid = add_user(email, pwhash)
     if uid:
-        return template("main", {"success": "Thanks for signing up.", "text": "Where are you headed?"})
+        update_session(email, uid)
+        return template("main", session_dict(success = "Thanks for signing up.", text = "Where are you headed?"))
     else:
-        return template("signup", {"error": "That email is already registered."})
-    
+        return template("signup", session_dict(error = "That email is already registered."))
+
+@route("/logout", method="GET")
+def logout():
+    clear_session()
+    return template("main", session_dict(info = "Come back soon.", text = "Where are you headed?"))
 
 ###########
 # Dump db info
@@ -142,5 +171,13 @@ if __name__ == "__main__":
         redis_host = os.environ.get("REDIS_HOST", "localhost")
         REDIS = redis.Redis(host=redis_host)
 
+    session_opts = {
+        'session.type': 'file',
+        'session.cookie_expires': 300,
+        'session.data_dir': './data',
+        'session.auto': True
+    }
+    app = SessionMiddleware(default_app(), session_opts)
+
     port = int(os.environ.get("PORT", 5000))
-    run(host='0.0.0.0', port=port)
+    run(host='0.0.0.0', port=port, app=app)
